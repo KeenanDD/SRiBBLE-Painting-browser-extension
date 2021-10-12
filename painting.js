13783b7a454cbd3734d70e96aa65be5fca5ccfed
KeenanDD <85482870+KeenanDD@users.noreply.github.com>
2021-10-12 21:30:21 +0700

    Add files via upload
    Initial release.

---
+++ painting.js
@@ -0,0 +1,82 @@
+charset="utf-8"
+var drawing = false;
+var context;
+
+
+window.addEventListener('load', function() {
+  //Clear screen
+  document.getElementById('btnClear').addEventListener('click', function() {
+    context.clearRect(0, 0, context.canvas.width, context.canvas.height);
+  }, false);
+
+  //Back btn
+  document.getElementById('btnBack').addEventListener('click', function() {
+    document.getElementById('myCanvas').style.display = "block";
+    document.getElementById('saveArea').style.display = "none";
+    document.getElementById('tools').style.display = "block";
+
+  }, false);
+
+  //Width Scale
+  document.getElementById('lineWidth').addEventListener('change', function() {
+    context.lineWidth = document.getElementById('lineWidth').value;
+  }, false);
+
+  //Colour
+  document.getElementById('colourChange').addEventListener('change', function() {
+    context.strokeStyle = document.getElementById('colourChange').value;
+  }, false);
+
+  //Save
+  document.getElementById('btnSave').addEventListener('click', function() {
+    document.getElementById('myCanvas').style.display = "none";
+    document.getElementById('saveArea').style.display = "block";
+    document.getElementById('tools').style.display = "none";
+
+    var dataURL = document.getElementById('myCanvas').toDataURL();
+    document.getElementById('canvasImg').src = dataURL;
+  }, false);
+
+  //Size Canvas
+  context = document.getElementById('myCanvas').getContext("2d");
+    context.canvas.width = window.innerWidth;
+    context.canvas.height = window.innerHeight;
+
+  //Mouse move
+  document.onmousemove = handleMouseMove;
+  document.onmousedown = handleDown;
+  document.onmouseup = handleUp;
+
+  //Style line
+  context.strokeStyle = "#000";
+  context.lineJoin = "round";
+  context.lineWidth = 5;
+
+  //Hide Save Area
+  document.getElementById('saveArea').style.display = "none";
+});
+
+function handleMouseMove(e) {
+  console.log(e.clientX);
+  console.log(e.clientY);
+  if (drawing) {
+
+    context.lineTo(e.clientX, e.clientY);
+    context.closePath();
+    context.stroke();
+    context.moveTo(e.clientX, e.clientY);
+  } else {
+    context.moveTo(e.clientX, e.clientY);
+  }
+}
+function handleDown(e) {
+  drawing = !drawing;
+  console.log(drawing);
+  context.moveTo(e.clientX, e.clientY);
+  context.beginPath();
+}
+
+function handleUp() {
+  drawing = !drawing;
+  console.log(drawing);
+}
